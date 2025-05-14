d10d5ba627a492a72e7f5536dd9bad64e158c5ca
whoisyounes <younes.al@northeastern.edu>
2025-05-13 21:02:14 -0400

    updated file3.cpp

--- hw1/file3.cpp
+++ hw1/file3.cpp
@@ -1,28 +1,48 @@
-/*  This program should determine if a number is a prime or composite number 
-    The program should ask user for a number 'num'
-    If 'num' is 0 or 1, print "num is not prime or composite"
-    For any value > 1, print "num is a prime number" or "num is a composite number" depending on the number */
+/*
+ * Q1C – Bugs fixed in file3.cpp:
+ *  1. Fixed header typo: changed <iosteam> to <iostream>.
+ *  2. Added `using namespace std;` so cout/cin resolve.
+ *  3. Replaced Python‑style `False` with C++ `false`.
+ *  4. Changed `if(num == 0 && num ==1)` to `if(num == 0 || num == 1)`.
+ *  5. Replaced colons in `if` and `for` headers with semicolons.
+ *  6. Fixed loop syntax to `for(int i = 2; i * i <= num; ++i)`.
+ *  7. Added parentheses in `if (num % i == 0)` test.
+ *  8. Unified variable name to `factor_found`.
+ *  9. Replaced `not factor_found` with `!factor_found`.
+ * 10. Ensured `main` returns 0 at the end.
+ */
 
-// Check for possible compiler errors, logical errors and rectify them
-// Re-factor the code by adding few comments (make it readable) and
-// provide list of most important fixes (in comments)
+#include <iostream>
+using namespace std;
 
-#include<iosteam>
+int main() {
+    int num;
+    bool factor_found = false;
 
-int main(void){
-    int num,i;
-    bool factor_found = False;
-    cout<<"Enter Number:\n";
-    cin>>num;
-    if(num == 0 && num ==1):
-        cout<<num<<" is not prime or composite"
-    for (i = 2, i<num, i++);{
-        if num%i == 0:
-            factor_Found = True
-            break;
+    // Prompt the user
+    cout << "Enter Number: ";
+    cin  >> num;
+
+    // 0 and 1 are neither prime nor composite
+    if (num == 0 || num == 1) {
+        cout << num << " is not prime or composite\n";
+        return 0;
     }
-    if not factor_found:
-        cout<<"is a composite number\n";
-    else:
-        cout<<"is a prime number\n")
+
+    // Check for divisors up to sqrt(num)
+    for (int i = 2; i * i <= num; ++i) {
+        if (num % i == 0) {
+            factor_found = true;
+            break;
+        }
+    }
+
+    // Output the result
+    if (!factor_found) {
+        cout << num << " is a prime number\n";
+    } else {
+        cout << num << " is a composite number\n";
+    }
+
+    return 0;
 }
